402e63aed56ae3ca0246d1a5536e70c00c535560
Ji Yoon Choi <chichoon.choi@gmail.com>
2021-02-26 01:40:44 +0900

    update readme.md

--- _nomad-python/0225/test.py
+++ _nomad-python/0225/test.py
@@ -11,3 +11,14 @@ from math import ceil
 #math 내의 ceil 함수만을 불러옴
 print(ceil(1.55555))
 #math.ceil 대신 ceil로 바로 사용 가능
+
+print("-------------requests-------------")
+import requests
+indeed_result = requests.get("https://github.com/chichoon")
+print(indeed_result)
+#response[200] -> 통신이 되었다는 의미
+
+print(indeed_result.text)
+#주소에 해당하는 모든 html 소스를 출력함
+
+import beautifulsoup4
