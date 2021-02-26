93599f1b8b54edda9932de78e3483f6295851eac
Ji Yoon Choi <chichoon.choi@gmail.com>
2021-02-27 01:37:31 +0900

    update codes

--- _nomad-python/0226/test.py
+++ _nomad-python/0226/test.py
@@ -6,15 +6,16 @@ soup = BeautifulSoup(result.text, "html.parser")
 
 print(soup.title) #Github page title
 pagination = soup.find("div", {"class": "pagination"})
+#div라는 키워드를 포함하고, class가 pagination인 요소 검색
 
-pages = pagination.find_all("a")
+links = pagination.find_all("a")
 #결과값이 담긴 list
 
-spans = []
-for page in pages:
-    spans.append(page.find("span"))
-#span이 포함된 page 내의 값들을 spans 리스트에 넣어줌
+pages = []
+for link in links[:-1]: #맨 끝의 값 제외
+    pages.append(int(link.find("span").string))
+#span이 포함된 page 내의 값들에 들어있는 string을 int로 변환하여 리스트에 저장
+print(pages[-1])
+#맨 마지막 원소만 출력
 
-del spans[-1]
-#맨 끝의 원소 삭제
-print(spans)
+max_page = pages[-1]
