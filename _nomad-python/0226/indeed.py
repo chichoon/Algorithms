b69b8e3cd3938ef3c0d2c068e627cbac16c77d40
Ji Yoon Choi <chichoon.choi@gmail.com>
2021-02-27 01:44:48 +0900

    update indeed.py

---
+++ _nomad-python/0226/indeed.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+INDEED_URL = "https://kr.indeed.com/jobs?q=python"
+
+def extract_indeed_pages():
+    result = requests.get(INDEED_URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+
+    print(soup.title) #Github page title
+    pagination = soup.find("div", {"class": "pagination"})
+    #div라는 키워드를 포함하고, class가 pagination인 요소 검색
+
+    links = pagination.find_all("a")
+    #결과값이 담긴 list
+
+    pages = []
+    for link in links[:-1]: #맨 끝의 값 제외
+        pages.append(int(link.find("span").string))
+    #span이 포함된 page 내의 값들에 들어있는 string을 int로 변환하여 리스트에 저장
+    print(pages[-1])
+    #맨 마지막 원소만 출력
+
+    max_page = pages[-1]
+    #총 페이지 수 (가장 큰 페이지 넘버)
