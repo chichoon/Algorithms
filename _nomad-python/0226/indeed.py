6880be35fb928f066da1db38485c81a781dc2028
Ji Yoon Choi <chichoon.choi@gmail.com>
2021-02-27 01:53:22 +0900

    update indeed.py

--- _nomad-python/0226/indeed.py
+++ _nomad-python/0226/indeed.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-INDEED_URL = "https://kr.indeed.com/jobs?q=python"
+LIMIT = 50
+URL = "https://kr.indeed.com/jobs?q=python"
 
 def extract_indeed_pages():
-    result = requests.get(INDEED_URL)
+    result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
 
     print(soup.title) #Github page title
@@ -18,8 +19,13 @@ def extract_indeed_pages():
     for link in links[:-1]: #맨 끝의 값 제외
         pages.append(int(link.find("span").string))
     #span이 포함된 page 내의 값들에 들어있는 string을 int로 변환하여 리스트에 저장
-    print(pages[-1])
-    #맨 마지막 원소만 출력
 
     max_page = pages[-1]
     #총 페이지 수 (가장 큰 페이지 넘버)
+    return max_page
+
+
+def extract_indeed_jobs(last_page):
+    for page in range(last_page):
+        print(f"&start={LIMIT * page}")
+
