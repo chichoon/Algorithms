97d063bca7eee2bc6f107ffbfddf41b3d6025c5c
Ji Yoon Choi <chichoon.choi@gmail.com>
2021-03-01 22:44:16 +0900

    update test.py

--- _nomad-python/0301/test.py
+++ _nomad-python/0301/test.py
@@ -5,6 +5,5 @@ from sof_func import get_jobs as get_sof_jobs
 #자주 쓸 코드를 함수화하여 불러옴
 
 indeed_jobs = get_indeed_jobs()
-print(indeed_jobs)
 sof_jobs = get_sof_jobs()
-print(sof_jobs)
+jobs = indeed_jobs + sof_jobs
