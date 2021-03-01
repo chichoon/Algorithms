df6e328df7ab65780a01b282e1112a30ce375f91
Ji Yoon Choi <chichoon.choi@gmail.com>
2021-03-02 00:02:52 +0900

    updated some file names

--- _nomad-python/0301/test.py
+++ _nomad-python/0301/test.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from indeed_func import get_jobs as get_indeed_jobs
 from sof_func import get_jobs as get_sof_jobs
 #자주 쓸 코드를 함수화하여 불러옴
-from save import save_to_file
+from save_func import save_to_file
 
 indeed_jobs = get_indeed_jobs()
 sof_jobs = get_sof_jobs()
