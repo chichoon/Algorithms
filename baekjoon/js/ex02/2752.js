5c59d74b468d94f4fc2d34f34dc0bf92b79a09f8
chichoon <chichoon.choi@gmail.com>
2022-01-09 22:17:14 +0900

    [백준] ADD: 9498 [JS]

---
+++ baekjoon/js/ex02/2752.js
@@ -0,0 +1,20 @@
+const wash = () => {
+  const fs = require("fs");
+  let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+  const arr = input[0].split(" ");
+  let ret = [];
+  for (var i in arr) {
+    ret.push(Number(arr[i]));
+  }
+  console.log(
+    `${ret
+      .sort((a, b) => {
+        if (a > b) return 1;
+        if (a === b) return 0;
+        if (a < b) return -1;
+      })
+      .join(" ")}`
+  );
+};
+
+wash();
