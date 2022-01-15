f43bc98f21c263904b21c00a926f332f8846d420
chichoon <chichoon.choi@gmail.com>
2022-01-15 22:53:51 +0900

    [백준] UPDATE: 2446 [JS]

--- baekjoon/js/ex02/2446.js
+++ baekjoon/js/ex02/2446.js
@@ -3,8 +3,8 @@ const star = () => {
   let input = parseInt(fs.readFileSync("/dev/stdin").toString().trim());
 
   for (let i = 0; i < input; i++) {
-    for (let j = 0; j < 2 * input; j++) {
-      if (j <= i || j + 1 >= 2 * input - i) {
+    for (let j = 1; j < 2 * input - i; j++) {
+      if (j <= i) {
         process.stdout.write(" ");
       } else {
         process.stdout.write("*");
@@ -13,8 +13,8 @@ const star = () => {
     process.stdout.write("\n");
   }
   for (let i = 1; i < input; i++) {
-    for (let j = 0; j < 2 * input; j++) {
-      if (j < input - i || j >= input + i) {
+    for (let j = 1; j < input + i + 1; j++) {
+      if (j < input - i) {
         process.stdout.write(" ");
       } else {
         process.stdout.write("*");
