492effc240482dc9a044d67c485ab870bbd64064
chichoon <chichoon.choi@gmail.com>
2022-01-15 22:38:47 +0900

    [백준] ADD: 2442 [JS]

---
+++ baekjoon/js/ex02/2442.js
@@ -0,0 +1,16 @@
+const star = () => {
+  const fs = require("fs");
+  let input = parseInt(fs.readFileSync("/dev/stdin").toString().trim());
+
+  for (let i = 1; i < input + 1; i++) {
+    for (let j = input - i; j > 0; j--) {
+      process.stdout.write(" ");
+    }
+    for (let j = 1; j < 2 * i; j++) {
+      process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
+};
+
+star();
