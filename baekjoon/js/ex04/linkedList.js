ee7212d330fdae3f5439c6c71e918123c35161c1
chichoon <chichoon.choi@gmail.com>
2022-01-18 17:40:21 +0900

    ADD: JS 연결리스트 야매구현

---
+++ baekjoon/js/ex04/linkedList.js
@@ -0,0 +1,78 @@
+class Node {
+  constructor(data, next = null, prev = null) {
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = new Node(-1);
+    this.tail = new Node(-1);
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+    this.size = 0;
+  }
+  insertBack(data) {
+    let node = new Node(data);
+    let currentNode;
+
+    if (!this.head) this.head = node;
+    else {
+      currentNode = this.head;
+      while (currentNode.next) currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    node.prev = currentNode;
+    node.next = this.tail;
+    this.size++;
+  }
+  insertNext(data, index) {
+    if (index < 0 || index > this.size) return;
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      this.size++;
+      return;
+    }
+    let node = new Node(data);
+    let currentNode;
+    let count = 0;
+
+    currentNode = this.head;
+    while (count < index) {
+      currentNode = currentNode.next;
+      count++;
+    }
+    node.next = currentNode.next;
+    node.prev = currentNode;
+    node.next.prev = node;
+    currentNode.next = node.next;
+    this.size++;
+  }
+  deleteThis(index) {
+    if (index < 0 || index > this.size) return;
+    let currentNode;
+    let count = 0;
+
+    currentNode = this.head;
+    while (count < index) {
+      currentNode = currentNode.next;
+      count++;
+    }
+    currentNode.prev.next = currentNode.next;
+    currentNode.next.prev = currentNode.prev;
+    this.size--;
+  }
+  printAll() {
+    let currentNode = this.head;
+
+    if (currentNode) process.stdout.write(currentNode.data);
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      process.stdout.write(currentNode.data);
+    }
+  }
+}
+
+export { node, LinkedList };
