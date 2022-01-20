d76a00f2b85e7c86d124afe03a9dc15ba4bd5073
chichoon <chichoon.choi@gmail.com>
2022-01-21 02:15:24 +0900

    RENAME: 폴더명 변경

--- baekjoon/js/ex02/1000.js
+++
@@ -1,9 +0,0 @@
-const aPlusB = () => {
-  const fs = require("fs");
-  let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-  const arr = input[0].split(" ");
-
-  console.log(`${Number(arr[0]) + Number(arr[1])}`);
-};
-
-aPlusB();
