b9695b5acca1be707ef156bdbed888aaf4b44f3f
chichoon <chichoon.choi@gmail.com>
2022-01-22 20:19:19 +0900

    [백준] ADD: 6549 [JS]

--- baekjoon/js/ex05_stack/6549.js
+++ baekjoon/js/ex05_stack/6549.js
@@ -1,20 +1,31 @@
 const histogram = () => {
   const fs = require("fs");
   let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-  let ans = [];
 
   for (line of input) {
     if (line === "0") break;
     let arr = line.split(" ").map((n) => {
       return parseInt(n);
     });
+    let n = arr[0];
+    arr.shift();
     let stack = [];
-    for (i of arr) {
-      while (stack.length > 0 && stack[stack.length - 1] > i) {}
-      stack.push(i);
+    let ans = 0;
+    for (let i = 0; i < n; i++) {
+      let idx = i;
+      while (stack.length > 0 && stack[stack.length - 1][0] >= arr[i]) {
+        let tmp = stack.pop();
+        if (ans < tmp[0] * (i - tmp[1])) ans = tmp[0] * (i - tmp[1]);
+        idx = tmp[1];
+      }
+      stack.push([arr[i], idx]);
     }
+    while (stack.length > 0) {
+      let tmp = stack.pop();
+      if (ans < tmp[0] * (n - tmp[1])) ans = tmp[0] * (n - tmp[1]);
+    }
+    console.log(ans);
   }
-  console.log(ans.join("\n"));
 };
 
 histogram();
