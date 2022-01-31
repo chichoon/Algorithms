c35f6394b3bdad09c912af28bb6d7556dfb7dd2f
chichoon <chichoon.choi@gmail.com>
2022-01-31 23:51:01 +0900

    [백준] ADD: 10757 [JS]

---
+++ baekjoon/js/ex02_implementation/10757.js
@@ -0,0 +1,11 @@
+const bigNum = () => {
+  let input = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split(" ")
+    .map(BigInt);
+  console.log((input[0] + input[1]).toString());
+};
+
+bigNum();
