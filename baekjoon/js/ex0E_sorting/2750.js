3cae3a5f4bc6dc6a1ed18a6838cbace2422aeca2
chichoon <chichoon.choi@gmail.com>
2022-02-05 11:59:42 +0900

    [백준] ADD: 2750 [JS]

---
+++ baekjoon/js/ex0E_sorting/2750.js
@@ -0,0 +1,12 @@
+const sort = () => {
+  let input = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split("\n")
+    .map(Number);
+  input.shift();
+  console.log(input.sort((a, b) => a - b).join("\n"));
+};
+
+sort();
