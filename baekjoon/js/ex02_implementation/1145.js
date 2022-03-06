8d95c769a95d3c386a0cde30b4e0a47bb65ca115
chichoon <chichoon.choi@gmail.com>
2022-03-06 12:45:53 +0900

    [백준] ADD: 1145 [JS]

---
+++ baekjoon/js/ex02_implementation/1145.js
@@ -0,0 +1,18 @@
+const atLast = () => {
+  let input = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split(" ")
+    .map(Number);
+  let i = 1;
+  while (true) {
+    let n = 0;
+    for (let j of input) if (i % j === 0) n++;
+    if (n >= 3) break;
+    i++;
+  }
+  console.log(i);
+};
+
+atLast();
