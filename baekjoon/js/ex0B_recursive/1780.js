db833f7411678351e6a6ace5ee2c20ce9bfa5b1b
chichoon <chichoon.choi@gmail.com>
2022-03-13 00:46:44 +0900

    [백준] ADD: 1780 [JS]

---
+++ baekjoon/js/ex0B_recursive/1780.js
@@ -0,0 +1,34 @@
+const recurPaper = (input, n, x, y, stack) => {
+  const cur = input[y][x];
+  if (n === 1) {
+    if (cur === -1) stack[0]++;
+    else if (cur === 0) stack[1]++;
+    else if (cur === 1) stack[2]++;
+    return;
+  }
+  let cnt = 0;
+  for (let i = y; i < y + n; i++)
+    for (let j = x; j < x + n; j++) if (input[i][j] === cur) cnt++;
+  if (cnt === n * n) stack[cur + 1]++;
+  else {
+    for (let i = 0; i < 3; i++)
+      for (let j = 0; j < 3; j++)
+        recurPaper(input, n / 3, x + (j * n) / 3, y + (i * n) / 3, stack);
+  }
+};
+
+const paper = () => {
+  let input = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split("\n")
+    .map((n) => n.split(" ").map(Number));
+  const n = input[0][0];
+  let stack = [0, 0, 0];
+  input.shift();
+  recurPaper(input, n, 0, 0, stack);
+  console.log(stack.join("\n"));
+};
+
+paper();
