a51a730fb8d2c542a1676235a4e1f917da7d1ec0
chichoon <chichoon.choi@gmail.com>
2022-03-24 22:36:01 +0900

    [백준] ADD: 15650 [JS]

---
+++ baekjoon/js/ex0c_backTracking/15650.js
@@ -0,0 +1,29 @@
+let arr;
+let n;
+let m;
+
+const recurNandM = (k, l, ans) => {
+  if (l > n + 1) return;
+  if (k === m) ans.push(arr.join(" "));
+  else {
+    for (let i = l; i <= n; i++) {
+      arr[k] = i;
+      recurNandM(k + 1, i + 1, ans);
+    }
+  }
+};
+
+const nAndM = () => {
+  [n, m] = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split(" ")
+    .map(Number);
+  let ans = [];
+  arr = Array.from({ length: m }, (v) => 0);
+  recurNandM(0, 1, ans);
+  console.log(ans.join("\n"));
+};
+
+nAndM();
