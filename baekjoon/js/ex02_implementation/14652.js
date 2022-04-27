2ff486b615e0c0ca15206fd9c3508eeeed34884b
chichoon <chichoon.choi@gmail.com>
2022-04-27 16:00:38 +0900

    [백준] ADD: 14652 [JS]

---
+++ baekjoon/js/ex02_implementation/14652.js
@@ -0,0 +1,11 @@
+const hanwha = () => {
+  let [n, m, k] = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split(" ")
+    .map(Number);
+  console.log(`${Math.floor(k / m)} ${k % m}`);
+};
+
+hanwha();
