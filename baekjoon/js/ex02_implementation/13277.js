f53937ad2582634eac0e5718779208151a923ae3
chichoon <chichoon.choi@gmail.com>
2022-05-23 01:21:10 +0900

    [백준] ADD: 13277 [JS]

---
+++ baekjoon/js/ex02_implementation/13277.js
@@ -0,0 +1,11 @@
+const big = () => {
+  const [a, b] = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split(" ")
+    .map(BigInt);
+  console.log((a * b).toString());
+};
+
+big();
