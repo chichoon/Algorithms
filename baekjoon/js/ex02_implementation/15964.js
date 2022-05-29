9b65fbb635a069eb9904565f5b4c6138cef8acf6
chichoon <chichoon.choi@gmail.com>
2022-05-29 22:32:47 +0900

    [백준] ADD: 15964 [JS]

---
+++ baekjoon/js/ex02_implementation/15964.js
@@ -0,0 +1,11 @@
+const math = () => {
+  let [a, b] = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split(" ")
+    .map(Number);
+  console.log((a + b) * (a - b));
+};
+
+math();
