4152fa4aa909f20c05de49a5ca5ea7dd41f55b54
chichoon <chichoon.choi@gmail.com>
2022-06-08 03:44:34 +0900

    [백준] ADD: 9086 [JS]

---
+++ baekjoon/js/ex02_implementation/9086.js
@@ -0,0 +1,5 @@
+let i = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+i.shift();
+let arr = [];
+for (let a of i) arr.push(`${a[0]}${a[a.length - 1]}`);
+console.log(arr.join("\n"));
