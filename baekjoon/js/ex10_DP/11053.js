c770877c328d2d8b06a495a2c07a0c498ee15bd6
chichoon <chichoon.choi@gmail.com>
2022-06-11 13:22:25 +0900

    [백준] UPDATE: 11053 코드 수 줄임 [JS]

--- baekjoon/js/ex10_DP/11053.js
+++ baekjoon/js/ex10_DP/11053.js
@@ -5,12 +5,10 @@ const long = () => {
     .trim()
     .split("\n")
     .map((v) => v.split(" ").map(Number));
-  let dp = Array.from({ length: n }, (v) => 0);
-  for (let i = 0; i < n; i++) {
-    dp[i] = 1;
+  let dp = Array.from({ length: n }, (v) => 1);
+  for (let i = 0; i < n; i++)
     for (let j = 0; j < i; j++)
       if (dp[j] >= dp[i] && arr[i] > arr[j]) dp[i] = dp[j] + 1;
-  }
   console.log(Math.max(...dp));
 };
 
