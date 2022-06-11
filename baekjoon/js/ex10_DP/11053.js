4d5b9ef3018474be868e85d9d2802e3424933237
chichoon <chichoon.choi@gmail.com>
2022-06-11 13:19:46 +0900

    [백준] ADD: 11053 [JS]

---
+++ baekjoon/js/ex10_DP/11053.js
@@ -0,0 +1,17 @@
+const long = () => {
+  const [[n], arr] = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split("\n")
+    .map((v) => v.split(" ").map(Number));
+  let dp = Array.from({ length: n }, (v) => 0);
+  for (let i = 0; i < n; i++) {
+    dp[i] = 1;
+    for (let j = 0; j < i; j++)
+      if (dp[j] >= dp[i] && arr[i] > arr[j]) dp[i] = dp[j] + 1;
+  }
+  console.log(Math.max(...dp));
+};
+
+long();
