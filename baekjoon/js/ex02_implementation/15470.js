f4faa667647ef55e1d22f28b2f94639606ad2df9
chichoon <chichoon.choi@gmail.com>
2022-06-19 22:37:34 +0900

    [백준] ADD: 15470 [JS]

---
+++ baekjoon/js/ex02_implementation/15470.js
@@ -0,0 +1,11 @@
+const plus = () => {
+  let [a, b] = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split(" ")
+    .map(BigInt);
+  console.log((a + b).toString());
+};
+
+plus();
