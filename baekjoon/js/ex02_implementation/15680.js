d331da33ec6980c95492ae6aa9aba794bcec1d01
chichoon <chichoon.choi@gmail.com>
2022-07-26 02:58:33 +0900

    [백준] ADD: 15680 [JS]

---
+++ baekjoon/js/ex02_implementation/15680.js
@@ -0,0 +1,6 @@
+const yonsei = () => {
+  let input = require("fs").readFileSync("/dev/stdin").toString().trim();
+  console.log(input === "0" ? "YONSEI" : "Leading the Way to the Future");
+};
+
+yonsei();
