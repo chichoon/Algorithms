5663c2f26a3d6b5b4456337b2c6cd9731d706f20
chichoon <chichoon.choi@gmail.com>
2022-08-02 01:05:14 +0900

    [백준] ADD: 10101 [JS]

---
+++ baekjoon/js/ex02_implementation/10101.js
@@ -0,0 +1,15 @@
+const triangle = () => {
+  const [a, b, c] = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split("\n")
+    .map(Number);
+  if (a === b && b === c) console.log("Equilateral");
+  else if (a + b + c === 180) {
+    if (a === b || b === c || a === c) console.log("Isosceles");
+    else console.log("Scalene");
+  } else console.log("Error");
+};
+
+triangle();
