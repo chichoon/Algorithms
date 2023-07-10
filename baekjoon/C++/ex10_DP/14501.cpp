096838fb32a27d8baf495093a0ff1ed580330935
chichoon <chichoon.choi@gmail.com>
2023-07-10 23:21:40 +0900

    [백준] ADD: 14501 [C++]

---
+++ baekjoon/C++/ex10_DP/14501.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(void) {
+	int n, max = -1;
+	int tArr[16] = {0};
+	int pArr[16] = {0};
+	int dp[16] = {0};
+
+	std::cin >> n;
+	for (int i = 0; i < n; i++) {
+		std::cin >> tArr[i] >> pArr[i];
+	}
+
+	for (int i = 0; i <= n; i++) {
+		int deadline = i + tArr[i];
+		dp[i] = std::max(dp[i], max);
+		if (deadline <= n) {
+			dp[deadline] = std::max(dp[deadline], dp[i] + pArr[i]);
+		}
+		max = std::max(dp[i], max);
+	}
+
+	std::cout << max << "\n";
+}
