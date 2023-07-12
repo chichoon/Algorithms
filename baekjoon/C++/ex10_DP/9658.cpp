87322fc2d555f40c2e06a49d7296cfd38ec8f479
chichoon <chichoon.choi@gmail.com>
2023-07-12 22:47:27 +0900

    [백준] ADD: 9658 [C++]

---
+++ baekjoon/C++/ex10_DP/9658.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(void) {
+	int n;
+	int dp[1001];
+
+	std::cin >> n;
+
+	dp[1] = 0;
+	dp[2] = 1;
+	dp[3] = 0;
+	dp[4] = 1;
+
+	for (int i = 5; i <= n; i++) {
+		if (dp[i - 1] == 1 && dp[i - 3] == 1 && dp[i - 4] == 1)
+			dp[i] = 0;
+		else dp[i] = 1;
+	}
+	std::cout << (dp[n] == 1 ? "SK" : "CY") << "\n";
+}
