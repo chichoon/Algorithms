0c7efec13727bda412d64120480b7103e9d2e191
chichoon <chichoon.choi@gmail.com>
2023-08-10 23:50:51 +0900

    [백준] ADD: 26574 [C++]

---
+++ baekjoon/C++/ex02_implementation/26574.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int    main(void)
+{
+    int    t, temp;
+    std::cin >> t;
+    for (int i = 0; i < t; i++) {
+        std::cin >> temp;
+        std::cout << temp << " " << temp << "\n";
+    }
+    return (0);
+}
