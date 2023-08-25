ae74edfbf5a6f643586f9b0a5f7ee92736ecfd7c
chichoon <chichoon.choi@gmail.com>
2023-08-26 00:28:50 +0900

    [백준] UPDATE: 2293 [C++]

--- baekjoon/C++/ex10_DP/2293.cpp
+++ baekjoon/C++/ex10_DP/2293.cpp
@@ -9,10 +9,10 @@ int main(void) {
 	std::cin >> n >> k;
 	for (int i = 0; i < n; i++) std::cin >> v[i];
 
+	dp[0] = 1;
 	for (int j = 0; j < n; j++) {
 		for (int i = 1; i <= k; i++) {
-			if (i == v[j]) dp[i] += 1;
-			if (i > v[j]) dp[i] = dp[i] + dp[i - v[j]];
+			if (i >= v[j]) dp[i] = dp[i] + dp[i - v[j]];
 		}
 	}
 
