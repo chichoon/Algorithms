efcaff95f90b114bed03f637999cf4e405ead6bf
chichoon <chichoon.choi@gmail.com>
2023-08-25 23:59:10 +0900

    [백준] ADD: 2293 [C++]

---
+++ baekjoon/C++/ex10_DP/2293.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int dp[10001];
+int v[101];
+
+int main(void) {
+	int n, k, temp;
+
+	std::cin >> n >> k;
+	for (int i = 0; i < n; i++) std::cin >> v[i];
+
+	for (int j = 0; j < n; j++) {
+		for (int i = 1; i <= k; i++) {
+			if (i == v[j]) dp[i] += 1;
+			if (i - v[j] >= 1) dp[i] = dp[i] + dp[i - v[j]];
+		}
+	}
+
+	std::cout << dp[k];
+}
