e413f004bb343ee50d4647371fd5ad37a3526772
chichoon <chichoon.choi@gmail.com>
2023-09-05 00:00:51 +0900

    [백준] UPDATE: 15904 [C++]

--- baekjoon/C++/ex11_greedy/15904.cpp
+++ baekjoon/C++/ex11_greedy/15904.cpp
@@ -3,15 +3,18 @@
 
 int main(void) {
 	std::string str;
-	std::string ans;
+	std::string ans = "UCPC";
 	int len;
+	int j = 0;
 
 	std::getline(std::cin, str);
 	len = str.length();
 
 	for (int i = 0; i < len; i++) {
-		if (str[i] >= 'A' && str[i] <= 'Z') ans += str[i];
+		if (str[i] == ans[j]) {
+			j++;
+		}
 	}
 
-	std::cout << (ans == "UCPC" ? "I love UCPC" : "I hate UCPC");
+	std::cout << (j == 4 ? "I love UCPC" : "I hate UCPC");
 }
