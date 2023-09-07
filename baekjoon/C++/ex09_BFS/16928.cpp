2d3a413d87e8c2d843d1c7fda9024c814a940baa
chichoon <chichoon.choi@gmail.com>
2023-09-07 23:32:01 +0900

    [백준] ADD: 16928 [C++]

---
+++ baekjoon/C++/ex09_BFS/16928.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+
+int n, m;
+int arr[30][2];
+int visited[101];
+std::queue<std::pair<int, int> > que;
+
+int bfs() {
+	que.push(std::make_pair(1, 0));
+	visited[1] = true;
+	bool flag;
+
+	while (!que.empty()) {
+		std::pair<int, int> current = que.front();
+		que.pop();
+		if (visited[current.first] == 0) visited[current.first] = current.second;
+		else visited[current.first] = std::min(visited[current.first], current.second);
+
+		for (int i = 1; i <= 6; i++) {
+			flag = false;
+			if (current.first + i == 100) return current.second + 1;
+			if (visited[current.first + i] != 0) continue;
+			for (int j = 0; j < n + m; j++) {
+				if (arr[j][0] == current.first + i) {
+					que.push(std::make_pair(arr[j][1], current.second + 1));
+					flag = true;
+					break;
+				}
+			}
+			if (!flag) que.push(std::make_pair(current.first + i, current.second + 1));
+		}
+	}
+
+	return -1;
+}
+
+int main(void) {
+	std::cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> arr[i][0] >> arr[i][1];
+	}
+
+	for (int i = n; i < n + m; i++) {
+		std::cin >> arr[i][0] >> arr[i][1];
+	}
+
+	std::cout << bfs();
+}
