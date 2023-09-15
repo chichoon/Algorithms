d1b1e1fd9c8f89526b4f144f216ea426c2cfd86d
chichoon <chichoon.choi@gmail.com>
2023-09-15 23:19:33 +0900

    [백준] ADD: 1106 [C++]

---
+++ baekjoon/C++/ex10_DP/1106.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::pair<int, int> > vec;
+int dp[10001];
+
+int main(void) {
+	int c, n, u, v, min;
+
+	std::cin >> c >> n;
+	for (int i = 0; i < n; i++) {
+		std::cin >> u >> v;
+		vec.push_back(std::make_pair(u, v));
+		dp[v] = std::min(u, dp[v]);
+	}
+
+	dp[0] = 0;
+	for (int i = 1; i <= 10001; i++) {
+		min = 1000000;
+		for (int j = 0; j < vec.size(); j++) {
+			if (i - vec[j].second < 0) continue;
+			min = std::min(dp[i - vec[j].second] + vec[j].first, min);
+			if (i % vec[j].second == 0) {
+				min = std::min(min, (i / vec[j].second) * vec[j].first);
+			}
+		}
+		dp[i] = min;
+	}
+
+	min = 1000000;
+	for (int i = c; i < 10001; i++) {
+		min = std::min(min, dp[i]);
+	}
+
+	std::cout << min;
+}
